6bebc124597c1e142c6e58643106cd29b84ea707
iamrajajamal <martian.hacked@gmail.com>
2019-03-30 20:07:44 +0500

    Added navigations through buttons in intro scene

---
+++ Assets/Scripts/CarSelection.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSelection : MonoBehaviour
+{
+    // We create an empty list of gameObjects
+    private GameObject[] carList;
+    private int currentCar = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Count the child gameObjects from the cars transform
+        carList = new GameObject[transform.childCount];
+
+        // Loop through the child items and fill the list in the current slots
+        for (int i = 0; i < transform.childCount; i++)
+            carList[i] = transform.GetChild(i).gameObject;
+
+        // Deactivate all the gameObjects in the list
+        foreach (GameObject gameObj in carList)
+            gameObj.SetActive(false);
+
+        // Set the initial GO to be active
+        if (carList[0])
+            carList[0].SetActive(true);
+    }
+
+    public void toggleCars(string direction)
+    {
+        carList[currentCar].SetActive(false);
+        if (direction == "Right")
+        {
+            currentCar++;
+            if (currentCar > carList.Length - 1)
+                currentCar = 0;
+        }
+        else if (direction == "Left")
+        {
+            currentCar--;
+            if (currentCar < 0)
+                currentCar = carList.Length - 1;
+        }
+
+        // Set the current car to be active from the list
+        carList[currentCar].SetActive(true);
+    }
+}
